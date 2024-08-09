43bd51e88c06319ad8c355ead07dfdf6128b1880
FortenZz0 <vanver2007@mail.ru>
2024-08-09 14:33:54 +0300

    Исправлен time_handler.py

--- handlers/time_handler.py
+++ handlers/time_handler.py
@@ -3,7 +3,7 @@ from netschoolapi import NetSchoolAPI
 from netschoolapi.schemas import Lesson
 import asyncio
 
-import days_handler
+from handlers import days_handler
 
 
 
@@ -12,14 +12,14 @@ import days_handler
 # 1 - сейчас урок. возвращается время до конца текущего урока
 # 2 - день кончился, уроков больше не будет. возвращается текущее время
 async def how_many_time_left(ns: NetSchoolAPI) -> tuple[int, time, Lesson | None]:
-    day = days_handler.get_current_day(ns)
+    day = await days_handler.get_current_day(ns)
     
-    current_time = datetime.now().time()
+    now = datetime.now()
     
     for i, lesson in enumerate(day.lessons):
-        lesson_start = datetime.combine(day.date, lesson.start)
-        lesson_end = datetime.combine(day.date, lesson.end)
-        current = datetime.combine(day.date, current_time)
+        lesson_start = datetime.combine(now.date(), lesson.start)
+        lesson_end = datetime.combine(now.date(), lesson.end)
+        current = datetime.combine(now.date(), now.time())
         
         # перемена. время до начала урока
         if lesson_start > current:
@@ -31,9 +31,9 @@ async def how_many_time_left(ns: NetSchoolAPI) -> tuple[int, time, Lesson | None
             return 0, lesson_start - current, next_lesson
         
         # урок. время до начала перемены
-        elif lesson.start <= current and lesson_end > current:
+        elif lesson_start <= current and lesson_end > current:
             return 1, lesson_end - current, lesson
         
         # после уроков. текущее время
-        elif lesson.end <= current and i == len(day.lessons) - 1:
+        elif lesson_end <= current and i == len(day.lessons) - 1:
             return 2, current.time(), None
