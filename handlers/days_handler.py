ce3b4eda8a4b4b76977f7acf5f810804b16ef0ae
FortenZz0 <vanver2007@mail.ru>
2024-08-09 13:29:21 +0300

    Создан days_handler.py

---
+++ handlers/days_handler.py
@@ -0,0 +1,20 @@
+from datetime import datetime, date, time
+from netschoolapi import NetSchoolAPI
+from netschoolapi.schemas import Lesson, Day
+import asyncio
+
+
+async def get_day(ns: NetSchoolAPI, day_date: date) -> Day:
+    today_diary = await ns.diary(start=date, end=date)
+    day = today_diary.schedule[0]
+    
+    return day
+
+
+async def get_current_day(ns: NetSchoolAPI) -> Day:
+    # today = date.today()
+    today = date(2024, 5, 20)
+    
+    day = get_current_day(ns, today)
+    
+    return day
