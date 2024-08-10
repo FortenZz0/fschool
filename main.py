3243c838501e6de11e2283d0a39a0e417c542465
FortenZz0 <vanver2007@mail.ru>
2024-08-10 09:20:40 +0300

    Подключён dotenv
    Убрал тестовые данные из main.py и заменил их на настоящие в файле .env
    
    Позже их тоже не будет, останется только BOT_TOKEN

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from datetime import datetime, date, time
 from netschoolapi import NetSchoolAPI
 import asyncio
 
+from dotenv import find_dotenv, load_dotenv
+from os import getenv
+
 from handlers import time_handler as time_h
 from handlers import days_handler as days_h
 from handlers import diary_handler as diary_h
@@ -9,14 +12,17 @@ from handlers import output_handler as out_h
 from handlers import marks_handler as marks_h
 
 
+denv = find_dotenv()
+load_dotenv(denv)
+
 
 async def main():
-    ns = NetSchoolAPI("https://sgo1.edu71.ru")
+    ns = NetSchoolAPI(getenv("MY_URL"))
     
     await ns.login(
-        'Верховцев',
-        '789964kOl)',
-        'МБОУ «ЦО № 34»'
+        getenv("MY_LOGIN"),
+        getenv("MY_PASS"),
+        getenv("MY_SCHOOLE_NAME")
     )
     
     print(await out_h.print_school_info(ns))
