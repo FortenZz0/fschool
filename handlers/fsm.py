f1cf813f51eafc2bde9540265b3d0e17e257b6fd
FortenZz0 <vanver2007@mail.ru>
2024-09-28 02:20:11 +0300

    Работа с машиной состояний

---
+++ handlers/fsm.py
@@ -0,0 +1,16 @@
+from aiogram.fsm.state import State, StatesGroup
+from aiogram.fsm.context import FSMContext
+
+
+
+# FSM для входа в аккаунт эл.ж.
+class LoginFSM(StatesGroup):
+    msg = State()
+    url = State()
+    login = State()
+    password = State()
+    school = State()
+    edit_url = State()
+    edit_login = State()
+    edit_password = State()
+    edit_school = State()
